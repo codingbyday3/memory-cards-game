dfbcdb00e9ba436231f12ac1493d9ef3a350e21b
Klemen <codingbyday3@users.noreply.github.com>
2025-07-28 09:06:14 +0200

    Build: place where score will be displayed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,25 @@
 
-function App() {
+function Score(){
+    return(
+        <section>
+            <p>Score: 12</p>
+            <p>Highest score: 20</p>
+        </section>
+    )
+}
 
+function App() {
+    return(
+        <>
+        <header>
+            <h1>Pokemone memory game</h1>
+        </header>
+        <main> 
+            <Score/>
+            {/* <Cards/> */}
+        </main>
+        </>
+    )
 }
 
 export default App
